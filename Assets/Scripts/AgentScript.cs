f669c571297c4f722511e193f15f10f03e8766e6
BrentVanDaele <149065557+BrentVanDaele@users.noreply.github.com>
2025-05-04 17:06:33 +0200

    human vision added to ai
    todo: shoot target, rewards, remove target. train ai

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -7,6 +7,8 @@ namespace Assets.Scripts
 {
     public class AgentScript : Agent
     {
+        [SerializeField] GameObject[] obstaclePrefabs;
+        private GameObject target;
         public override void CollectObservations(VectorSensor sensor)
         {
             sensor.AddObservation(this.transform.localRotation);
@@ -14,12 +16,45 @@ namespace Assets.Scripts
 
         public override void OnActionReceived(ActionBuffers actions)
         {
-            base.OnActionReceived(actions);
+            Vector3 directionToLook = this.RayCastPeripheralVision(220f, 135f, 30, 10);
+            this.transform.rotation = Quaternion.LookRotation(directionToLook);
         }
 
         public override void OnEpisodeBegin()
         {
-            
+            target = Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)]);
+        }
+
+        // replicates complete human vision to find target
+        private Vector3 RayCastPeripheralVision(float fovHorizontal, float fovVertical, short precisionAmountRays, short depth)
+        {
+            float fovHorizontalCenter = fovHorizontal / 2;
+            float fovVerticalCenter = fovVertical / 2;
+            Vector3 faceDirection = this.transform.position;
+
+            for (int i = 0; i < precisionAmountRays; i++)
+            {
+                // devide fov with the presisionAmountRays to slowly turn from side to side via the loop
+                float angleHorizontal = Mathf.Lerp(-fovHorizontalCenter, fovHorizontalCenter, (float)i / (precisionAmountRays - 1));
+                for (int j = 0; j < precisionAmountRays; j++)
+                {
+                    // devide fov with the presisionAmountRays to slowly turn from up to down via the loop
+                    float angleVertical = Mathf.Lerp(-fovVerticalCenter, fovVerticalCenter, (float)j / (precisionAmountRays - 1));
+
+                    Vector3 direction = Quaternion.Euler(angleVertical, angleHorizontal, 0) * faceDirection;
+                    this.transform.rotation = Quaternion.LookRotation(direction); // move with looking to make realistic
+                    if (Physics.Raycast(faceDirection, direction, out RaycastHit hit, depth) && hit.collider.CompareTag("target"))
+                    {
+                        Debug.DrawLine(faceDirection, hit.point, Color.yellow);
+                        return direction;
+                    }
+                    else
+                    {
+                        Debug.DrawRay(faceDirection, direction * depth, Color.gray);
+                    }
+                }
+            }
+            return faceDirection;
         }
     }
 }
--- Assets/Scripts/TargetScript.cs
+++ Assets/Scripts/TargetScript.cs
@@ -1,23 +1,38 @@
 using UnityEngine;
 
-public class TargetScript : MonoBehaviour
+namespace Assets.Scripts
 {
-    [SerializeField] short targetValue = 1;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class TargetScript : MonoBehaviour
     {
-        
-    }
+        [SerializeField] short targetValue = 1;
+        [SerializeField] short minX = 0;
+        [SerializeField] short minY = 0;
+        [SerializeField] short minZ = 0;
+        [SerializeField] short maxX = 0;
+        [SerializeField] short maxY = 0;
+        [SerializeField] short maxZ = 0;
+        private float x = 0;
+        private float y = 0;
+        private float z = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        // maybe for future movement?
-    }
+        void Start()
+        {
+            x = Random.Range(minX, maxX);
+            y = Random.Range(minY, maxY);
+            z = Random.Range(minZ, maxZ);
+            transform.localPosition = new Vector3(x, y, z);
+        }
 
-    short Hit()
-    {
-        Destroy(this);
-        return targetValue;
+        // Update is called once per frame
+        void Update()
+        {
+            // maybe for future movement?
+        }
+
+        short Hit()
+        {
+            Destroy(this);
+            return targetValue;
+        }
     }
 }

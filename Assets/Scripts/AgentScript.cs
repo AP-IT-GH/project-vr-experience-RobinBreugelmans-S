e9696e55b238719df56d63b68fb3ccd271a9089f
BrentVanDaele <149065557+BrentVanDaele@users.noreply.github.com>
2025-05-10 12:22:10 +0200

    update ray casting

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -27,7 +27,7 @@ namespace Assets.Scripts
 
         public override void CollectObservations(VectorSensor sensor)
         {
-            sensor.AddObservation(this.transform.forward);
+            sensor.AddObservation(this.transform.forward); // own position
         }
 
         public override void OnActionReceived(ActionBuffers actions)
@@ -36,10 +36,9 @@ namespace Assets.Scripts
             float vertical = actions.ContinuousActions[1]; // up/down
             bool targetFound = false;
 
-            // let agent rotate
-            Quaternion rotate = Quaternion.LookRotation(Vector3.up * horizontal + Vector3.right * vertical);
-            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotate, rotationSpeed * Time.deltaTime);
-
+            // rotate the agent based on ml input
+            this.transform.Rotate(Vector3.up, horizontal * rotationSpeed * Time.deltaTime);
+            this.transform.Rotate(Vector3.right, -vertical * rotationSpeed * Time.deltaTime);
 
             if (Physics.Raycast(this.transform.position, this.transform.forward, out RaycastHit hit, fovDepth))
             {
@@ -58,7 +57,7 @@ namespace Assets.Scripts
                         {
                             Quaternion rotationToTarget = Quaternion.LookRotation(hit.transform.position);
                             this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotationToTarget, rotationSpeed * Time.deltaTime);
-                            Shoot(hit);
+                            Shoot();
                             targetFound = true;
                             SetReward(2.0f);
                         }
@@ -71,77 +70,15 @@ namespace Assets.Scripts
             }
         }
 
-        void Shoot(RaycastHit hit)
+        void Shoot()
         {
             //TODO: replace the hit with an actual shot from gun (Robin's code)
-            int score = hit.collider.GetComponent<TargetScript>().Hit();
-            ScoreScript.AddScoreML(score);
+            //this.GetComponentInChildren<Gun>().shoot();
+            if (Physics.Raycast(this.transform.position, this.transform.forward, out RaycastHit hit, fovDepth))
+            {
+                int score = hit.collider.GetComponent<TargetScript>().Hit();
+                ScoreScript.AddScoreML(score);
+            }
         }
-
-        //public override void OnActionReceived(ActionBuffers actions)
-        //{
-        //    // check if target found
-        //    if (this.RayCastPeripheralVision(out Vector3 directionToLook))
-        //    {
-        //        // rotate to target at human pace
-        //        Quaternion rotateDirection = Quaternion.LookRotation(directionToLook);
-        //        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotateDirection, rotationSpeed * Time.deltaTime);
-        //        // 'shoot' target
-        //        Physics.Raycast(directionToLook, directionToLook, out RaycastHit hit, fovDepth);
-        //        for (int i = 0; i < obstaclePrefabs.Length; i++)
-        //        {
-        //            // if target hit, check tag to be sure
-        //            if (hit.collider.CompareTag(obstaclePrefabs[i].tag))
-        //            {
-        //                // add score from target to ml score
-        //                int score = hit.collider.gameObject.GetComponent<TargetScript>().Hit();
-        //                ScoreScript.AddScoreML(score);
-        //            }
-        //        }
-        //    }
-        //    else
-        //    {
-        //        // if no target found, reset to begin position at human pace
-        //        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, startRotation, rotationSpeed * Time.deltaTime);
-        //    }
-        //}
-
-        //// replicates complete human vision to find target
-        //private bool RayCastPeripheralVision(out Vector3 directionToLook)
-        //{
-        //    float fovHorizontalCenter = fovHorizontal / 2;
-        //    float fovVerticalCenter = fovVertical / 2; // center of fov for both vertical and horizontal
-        //    Vector3 faceDirection = this.transform.forward; //get current direcction the agent is facing
-
-        //    for (int i = 0; i < precisionAmountRays; i++)
-        //    {
-        //        // devide fov with the presisionAmountRays to slowly turn from side to side via the loop
-        //        float angleHorizontal = Mathf.Lerp(-fovHorizontalCenter, fovHorizontalCenter, (float)i / (precisionAmountRays - 1));
-        //        for (int j = 0; j < precisionAmountRays; j++)
-        //        {
-        //            // devide fov with the presisionAmountRays to slowly turn from up to down via the loop
-        //            float angleVertical = Mathf.Lerp(-fovVerticalCenter, fovVerticalCenter, (float)j / (precisionAmountRays - 1));
-        //            // calculate direction to look and possible rotate
-        //            Vector3 direction = Quaternion.Euler(angleVertical, angleHorizontal, 0) * faceDirection;
-        //            // check if target at that direction
-        //            for (int k = 0; k < obstaclePrefabs.Length; k++)
-        //            {
-        //                if (Physics.Raycast(this.transform.position, direction, out RaycastHit hit, fovDepth) && hit.collider.CompareTag(obstaclePrefabs[k].tag))
-        //                {
-        //                    // if target then finish looking
-        //                    Debug.DrawLine(faceDirection, hit.point, Color.yellow);
-        //                    directionToLook = direction;
-        //                    return true;
-        //                }
-        //                else
-        //                {
-        //                    Debug.DrawRay(faceDirection, direction * fovDepth, Color.gray);
-        //                }
-        //            }
-        //        }
-        //    }
-        //    directionToLook = Vector3.zero;
-        //    return false;
-        //}
     }
 }

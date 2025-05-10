4e8bbae4ee28b4a65d76654491f82dc021a78ac9
BrentVanDaele <149065557+BrentVanDaele@users.noreply.github.com>
2025-05-10 20:47:40 +0200

    failed ai

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using static Unity.MLAgents.Sensors.RayPerceptionOutput;
+using UnityEngine.InputSystem;
 
 namespace Assets.Scripts
 {
@@ -9,76 +11,101 @@ namespace Assets.Scripts
     {
         [SerializeField] GameObject[] obstaclePrefabs; // the targets to look for
         [SerializeField] short fovDepth = 10; // max distance to look at
-        [SerializeField] float fovHorizontal = 220f; // human horisontal fov
-        [SerializeField] float fovVertical = 135f; // human vertical fov
-        [SerializeField] short precisionAmountRays = 30; // for the presision of finding a target
         [SerializeField] float rotationSpeed = 90f; // speed to rotate ML agent head to mach human speed
-        Quaternion startRotation;
         GameObject target;
+        Quaternion og_rotation; // to store initial rotation to compare its current rotation to and give penalty when rotating out of gunrange
 
         private void Start()
         {
-            startRotation = this.transform.rotation;
+            og_rotation = this.transform.rotation;
         }
         public override void OnEpisodeBegin()
         {
+            if (target != null)
+            {
+                Destroy(target);
+            }
             target = Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)]);
         }
 
         public override void CollectObservations(VectorSensor sensor)
         {
-            sensor.AddObservation(this.transform.forward); // own position
+            sensor.AddObservation(this.transform.forward); // own forward position
+            sensor.AddObservation(this.transform.rotation); // own rotation
+            Vector3 og_forward = og_rotation * Vector3.forward;
+            float alignment = Vector3.Dot(this.transform.forward, og_forward);
+            sensor.AddObservation(alignment); // value between -1 and 1
         }
 
         public override void OnActionReceived(ActionBuffers actions)
         {
             float horizontal = actions.ContinuousActions[0];   // left/right
             float vertical = actions.ContinuousActions[1]; // up/down
-            bool targetFound = false;
 
             // rotate the agent based on ml input
             this.transform.Rotate(Vector3.up, horizontal * rotationSpeed * Time.deltaTime);
             this.transform.Rotate(Vector3.right, -vertical * rotationSpeed * Time.deltaTime);
 
-            if (Physics.Raycast(this.transform.position, this.transform.forward, out RaycastHit hit, fovDepth))
-            {
-                for (int i = 0; i < obstaclePrefabs.Length; i++) {
-                    if (hit.collider.CompareTag(obstaclePrefabs[i].tag))
+
+            // Soft rotation penalty based on deviation from original forward
+            Vector3 og_forward = og_rotation * Vector3.forward;
+            float alignment = Vector3.Dot(this.transform.forward.normalized, og_forward.normalized); // looks for difference in angle and puts it between -1 and 1
+            float anglePenalty = 1f - Mathf.Clamp01((alignment + 1f) / 2f); // Penalty increases as misalignment grows
+            AddReward(-anglePenalty * 0.005f);
+
+            // Reward for aligning with original forward direction
+            float angleReward = Mathf.Clamp01((alignment + 1f) / 2f); // reward increases as alligment grows
+            AddReward(angleReward * 0.005f);
+
+
+            RayPerceptionSensorComponent3D[] sensors = GetComponentsInChildren<RayPerceptionSensorComponent3D>();
+            bool targetDetected = false;
+
+            foreach (RayPerceptionSensorComponent3D sensor in sensors){
+                RayPerceptionOutput output = RayPerceptionSensor.Perceive(sensor.GetRayPerceptionInput(), true);
+                foreach (RayOutput ray in output.RayOutputs)
+                {
+                    if (ray.HasHit && ray.HitGameObject != null && ray.HitGameObject.layer == 6) // Check if the ray hit the target
                     {
-                        Vector3 directionToTarget = (hit.point - this.transform.position).normalized;
-                        // Calculate the angle between agent's forward direction and target direction (horizontal check)
-                        float horizontalAngle = Vector3.Angle(new Vector3(this.transform.forward.x, 0, this.transform.forward.z), new Vector3(directionToTarget.x, 0, directionToTarget.z));
-
-                        // Calculate the vertical angle (check for vertical FOV)
-                        float verticalAngle = Vector3.Angle(new Vector3(0, this.transform.forward.y, 0), new Vector3(0, directionToTarget.y, 0));
-
-                        // Check if the target is within the horizontal FOV
-                        if (horizontalAngle < fovHorizontal / 2f && verticalAngle < fovVertical / 2f)
-                        {
-                            Quaternion rotationToTarget = Quaternion.LookRotation(hit.transform.position);
-                            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotationToTarget, rotationSpeed * Time.deltaTime);
-                            Shoot();
-                            targetFound = true;
-                            SetReward(2.0f);
-                        }
+                        targetDetected = true;
+                        break;
                     }
                 }
             }
-            if (!targetFound)
+            
+
+            if (targetDetected)
             {
-                SetReward(-0.1f);
+                AddReward(0.1f); // Reward agent for keeping the target in sight
+            }
+
+            if (Physics.Raycast(this.transform.position, this.transform.forward, out RaycastHit hit, fovDepth) && hit.transform.gameObject.layer == 6)
+            {
+                Shoot();
+                AddReward(2.0f);
+                EndEpisode();
+            }
+
+            // useless step punishment
+            AddReward(-0.001f);
+            
+            if (StepCount > 500)
+            {
+                AddReward(-1f); // punishment for not finding target withing given steps
+                EndEpisode();
             }
         }
 
         void Shoot()
         {
-            //TODO: replace the hit with an actual shot from gun (Robin's code)
-            //this.GetComponentInChildren<Gun>().shoot();
-            if (Physics.Raycast(this.transform.position, this.transform.forward, out RaycastHit hit, fovDepth))
-            {
-                int score = hit.collider.GetComponent<TargetScript>().Hit();
-                ScoreScript.AddScoreML(score);
-            }
+            int score = this.GetComponentInChildren<Gun>().Shoot();
+            ScoreScript.AddScoreML(score);
+
+            //if (Physics.Raycast(this.transform.position, this.transform.forward, out RaycastHit hit, fovDepth))
+            //{
+            //    int score = hit.collider.GetComponent<TargetScript>().Hit();
+            //    ScoreScript.AddScoreML(score);
+            //}
         }
     }
 }
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,74 +4,80 @@ using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class Gun : MonoBehaviour
+namespace Assets.Scripts
 {
-
-    public InputActionReference activateL;
-    public InputActionReference activateR;
-    public NearFarInteractor interactor = null;
-    private bool selected = false;
-
-    [SerializeField]
-    private Material bulletHoleMaterial;
-
-    private Transform fire;
-
-    void Start()
+    public class Gun : MonoBehaviour
     {
-        interactor.selectEntered.AddListener(OnGrab);
-        interactor.selectExited.AddListener(OnExit);
 
-        activateL.action.performed += Use;
-        activateR.action.performed += Use;
+        public InputActionReference activateL;
+        public InputActionReference activateR;
+        public NearFarInteractor interactor = null;
+        private bool selected = false;
 
-        fire = transform.GetChild(1).transform;
-    }
+        [SerializeField]
+        private Material bulletHoleMaterial;
 
-    private void Use(CallbackContext callback)
-    {
-        if (!selected)
-            return;
+        private Transform fire;
 
-        shoot();
-    }
-
-    private void shoot()
-    {
-        //ammmo--;
-        RaycastHit hit;
-        if (Physics.Raycast(fire.position, transform.forward, out hit, 2048f)) //, maxDistance
+        void Start()
         {
-            //layer 6 = Target layer
-            if (hit.transform.gameObject.layer == 6)
-            {
-                //give points, etc.
-                print("hit target");
-            }
-            else
-            {
-                GameObject bulletHole = GameObject.CreatePrimitive(PrimitiveType.Quad);
-                Destroy(bulletHole.GetComponent<Collider>());
-                bulletHole.transform.rotation = Quaternion.FromToRotation(Vector3.back, hit.normal);
-                bulletHole.transform.localScale = new Vector3(.11f, .11f, 1f);
-                bulletHole.GetComponent<Renderer>().material = bulletHoleMaterial;
-                bulletHole.transform.position = hit.point + hit.normal * .001f;
-                Destroy(bulletHole, 90); //90s
-            }
+            interactor.selectEntered.AddListener(OnGrab);
+            interactor.selectExited.AddListener(OnExit);
+
+            activateL.action.performed += Use;
+            activateR.action.performed += Use;
+
+            fire = transform.GetChild(1).transform;
         }
-    }
 
-    private void OnExit(SelectExitEventArgs arg0)
-    {
-        selected = false;
-    }
-
-    private void OnGrab(SelectEnterEventArgs arg0)
-    {
-        if (arg0.interactableObject.transform.gameObject == gameObject)
+        private void Use(CallbackContext callback)
         {
-            selected = true;
+            if (!selected)
+                return;
 
+            Shoot();
+        }
+
+        public int Shoot()
+        {
+            //ammmo--;
+            RaycastHit hit;
+            if (Physics.Raycast(fire.position, transform.forward, out hit, 2048f)) //, maxDistance
+            {
+                //layer 6 = Target layer
+                if (hit.transform.gameObject.layer == 6)
+                {
+                    //give points, etc.
+                    print("hit target");
+                    // return the score
+                    return hit.collider.GetComponent<TargetScript>().Hit();
+                }
+                else
+                {
+                    GameObject bulletHole = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                    Destroy(bulletHole.GetComponent<Collider>());
+                    bulletHole.transform.rotation = Quaternion.FromToRotation(Vector3.back, hit.normal);
+                    bulletHole.transform.localScale = new Vector3(.11f, .11f, 1f);
+                    bulletHole.GetComponent<Renderer>().material = bulletHoleMaterial;
+                    bulletHole.transform.position = hit.point + hit.normal * .001f;
+                    Destroy(bulletHole, 90); //90s
+                }
+            }
+            return 0;
+        }
+
+        private void OnExit(SelectExitEventArgs arg0)
+        {
+            selected = false;
+        }
+
+        private void OnGrab(SelectEnterEventArgs arg0)
+        {
+            if (arg0.interactableObject.transform.gameObject == gameObject)
+            {
+                selected = true;
+
+            }
         }
     }
 }

ff5c2df87a168cc07e048cf62bb2a46d02640a60
BrentVanDaele <149065557+BrentVanDaele@users.noreply.github.com>
2025-05-13 16:48:31 +0200

    agent fov update and small fixes to agent code

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -30,16 +30,19 @@ namespace Assets.Scripts
         {
             sensor.AddObservation(this.transform.forward); // own horisontal position
             sensor.AddObservation(this.transform.up); // own vertical position
+            sensor.AddObservation(-aiGun.transform.right); // gun horisontal
+            sensor.AddObservation(aiGun.transform.up); // gun vertical
         }
 
         public override void OnActionReceived(ActionBuffers actions)
         {
-            float horizontal = actions.ContinuousActions[0];   // left/right
+            float horizontal = actions.ContinuousActions[0]; // left/right
             float vertical = actions.ContinuousActions[1]; // up/down
 
             // rotate the agent based on ml input
             this.transform.Rotate(Vector3.up, horizontal * rotationSpeed * Time.deltaTime);
             this.transform.Rotate(Vector3.right, -vertical * rotationSpeed * Time.deltaTime);
+            SetReward(-0.0001f); //small punishment that will be irrelevant if target is in view, see further, this will hopefully dicurage random steps
 
             // Collect all available sensors for raycasting that are located in the childeren from the agent object.
             // This way you can add and remove as fit without needing to adjust the script.
@@ -60,19 +63,27 @@ namespace Assets.Scripts
                         break;
                     }
                 }
+                if (targetDetected)
+                {
+                    break;
+                }
             }
 
 
             if (targetDetected)
             {
-                float angleBetweenAgentTarget = Vector3.Dot(-aiGun.transform.right, (hitObject.transform.position - aiGun.transform.position).normalized); // find the angel (from -1 to 1) between the gun and target 
+                float angleBetweenAgentTarget = Vector3.Dot(-aiGun.transform.right, (hitObject.transform.position - aiGun.transform.position).normalized); // find the angel (from -1 to 1) between the gun and target, 1 should be strait before the gun
                 if (angleBetweenAgentTarget > 0.8f) // do not reward random angles, only when truly close
                 {
-                    SetReward(0.5f);//SetReward(angleBetweenAgentTarget * 0.3f); // Give a reward as long as the target is detected and is closet by the 1 degrees angle ( so if its strait before the gun).
+                    SetReward(angleBetweenAgentTarget * 0.3f); // Give a reward as long as the target is detected and is closet by the 1 degrees angle ( so if its strait before the gun).
                 }
-                if (angleBetweenAgentTarget > 0.5f) // smaller reward for bigger angel, but still reasonable
+                else if (angleBetweenAgentTarget > 0.5f) // smaller reward for bigger angel, but still reasonable
                 {
-                    SetReward(0.1f);//SetReward(angleBetweenAgentTarget * 0.05f);
+                    SetReward(angleBetweenAgentTarget * 0.1f);
+                }
+                else
+                {
+                    SetReward(angleBetweenAgentTarget * 0.001f);
                 }
                 if (Physics.Raycast(aiGun.transform.position, -aiGun.transform.right, out RaycastHit hit, fovDepth) && hit.transform.gameObject.layer == 6) // Direction is -right case thats how the gun prefab from the asset store is orientated
                 {

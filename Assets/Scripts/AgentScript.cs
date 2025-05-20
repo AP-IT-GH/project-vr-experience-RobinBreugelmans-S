97544f4616a263a74bad0bf7d2988f566e5e2a52
BrentVanDaele <149065557+BrentVanDaele@users.noreply.github.com>
2025-05-20 14:23:52 +0200

    GOOD CODE, can better tho

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -19,6 +19,7 @@ namespace Assets.Scripts
         public override void OnEpisodeBegin()
         {
             this.transform.localRotation = Quaternion.Euler(0, -90, 0);
+            this.currentShotCount = 0;
         }
 
         public override void CollectObservations(VectorSensor sensor)
@@ -34,35 +35,35 @@ namespace Assets.Scripts
         public override void OnActionReceived(ActionBuffers actions)
         {
             float horizontal = actions.ContinuousActions[0];
-            int shoot = actions.DiscreteActions[0];
+            float shoot = actions.ContinuousActions[1];
+            Debug.Log($"shoot: {shoot}, horizontal: {horizontal}");
 
             transform.Rotate(Vector3.up * horizontal * rotationSpeed * Time.deltaTime);
             SetReward(-0.001f);
 
-            if (shoot == 1 && currentShotCount < maxShots)
+            if (shoot >= 0.5f && currentShotCount < maxShots)
             {
+                Debug.Log("Shooting");
                 currentShotCount++;
-                SetReward(-0.01f);
-                if (Physics.Raycast(aiGun.transform.position, -aiGun.transform.right, out RaycastHit rayHit, fovDistance) && rayHit.transform.gameObject.layer == layerOfTarget)
+                SetReward(0.001f);
+                Debug.DrawRay(aiGun.transform.position, -aiGun.transform.right*fovDistance, Color.red);
+                if (Physics.Raycast(aiGun.transform.position, -aiGun.transform.right, out RaycastHit hit, fovDistance) && hit.transform.gameObject.layer == 6)
                 {
+                    Debug.Log("Hit");
                     SetReward(2.0f);
-                    ScoreScript.AddScoreML(rayHit.transform.gameObject.GetComponent<TargetScript>().Hit());
+                    ScoreScript.AddScoreML(hit.transform.gameObject.GetComponent<TargetScript>().Hit());
                     EndEpisode();
                 }
             }
-            else if (shoot != 0 && currentShotCount < maxShots)
+            else if (shoot < 0.5f && currentShotCount < maxShots)
             {
-                SetReward(0.01f);
+                SetReward(-0.01f);
             }
             else
             {
                 SetReward(-1.0f);
                 EndEpisode();
             }
-        }
-
-        public override void Heuristic(in ActionBuffers actionsOut)
-        {
         }
 
         public void SetNewTarget(GameObject target)
